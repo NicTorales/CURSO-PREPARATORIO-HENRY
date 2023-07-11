a7f1c7be42da7a1e259dc0d03c82e2d21cbfbd5d
NicTorales <ayalatoralesnicolas@gmail.com>
2023-07-12 01:30:37 +0200

    CURSO PHEN con bucles

---
+++ bucles.js
@@ -0,0 +1,16 @@
+var suma = 0
+
+
+while(suma < 3) {
+    suma = suma + 1;
+        console.log(suma);
+}
+
+//var 'caca' porque sino Variable suma daria '13'
+var caca = 0
+
+for (let i = 0; i < 5; i++) {
+    caca = caca + i;
+    console.log('Variable de iteración: ', i);
+ }
+ console.log('Variable suma: ', caca);
